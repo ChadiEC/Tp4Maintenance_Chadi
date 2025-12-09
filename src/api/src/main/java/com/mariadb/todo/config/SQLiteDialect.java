2d54081cd28c947ea538e766380591cede19ef78
ChadiEC <1985081@collegemv.qc.ca>
2025-12-09 13:04:38 -0500

    Initial commit - TP4 Maintenance conteneurisation

---
+++ src/api/src/main/java/com/mariadb/todo/config/SQLiteDialect.java
@@ -0,0 +1,26 @@
+package com.mariadb.todo.config;
+
+import org.hibernate.dialect.Dialect;
+import org.hibernate.dialect.function.StandardSQLFunction;
+import org.hibernate.type.StringType;
+
+public class SQLiteDialect extends Dialect {
+    public SQLiteDialect() {
+        registerFunction("lower", new StandardSQLFunction("lower", StringType.INSTANCE));
+    }
+
+    @Override
+    public boolean dropConstraints() {
+        return false;
+    }
+
+    @Override
+    public boolean hasAlterTable() {
+        return false;
+    }
+
+    @Override
+    public boolean qualifyIndexName() {
+        return false;
+    }
+}
