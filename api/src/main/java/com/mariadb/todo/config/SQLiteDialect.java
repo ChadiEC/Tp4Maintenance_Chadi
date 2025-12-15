3d0e6f4f087590a81b3335caffb4c00d76e3ffc3
Chadi El Chami <1985081@collegemv.qc.ca>
2025-12-15 11:49:26 -0500

    passer a java 21

--- api/src/main/java/com/mariadb/todo/config/SQLiteDialect.java
+++ api/src/main/java/com/mariadb/todo/config/SQLiteDialect.java
@@ -3,14 +3,14 @@ package com.mariadb.todo.config;
 import org.hibernate.dialect.Dialect;
 import org.hibernate.dialect.function.StandardSQLFunction;
 import org.hibernate.type.StandardBasicTypes;
-import org.hibernate.dialect.function.SQLFunctionRegistry; 
+import org.hibernate.query.sqm.function.FunctionRegistry; 
 
 public class SQLiteDialect extends Dialect {
     
+
     @Override
-    protected void initializeFunctionRegistry(SQLFunctionRegistry functionRegistry) {
+    protected void initializeFunctionRegistry(FunctionRegistry functionRegistry) { 
         super.initializeFunctionRegistry(functionRegistry);
-        
         
         functionRegistry.register(
             "lower", 
