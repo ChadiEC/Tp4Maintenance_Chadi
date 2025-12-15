63321249d33fbee7621d49687b63147d82b55497
Chadi El Chami <1985081@collegemv.qc.ca>
2025-12-15 11:55:00 -0500

    passer a java 21

--- api/src/main/java/com/mariadb/todo/config/SQLiteDialect.java
+++ api/src/main/java/com/mariadb/todo/config/SQLiteDialect.java
@@ -1,40 +1,51 @@
 package com.mariadb.todo.config;
 
+import java.sql.Types;
 import org.hibernate.dialect.Dialect;
 import org.hibernate.dialect.function.StandardSQLFunction;
 import org.hibernate.type.StandardBasicTypes;
-import org.hibernate.query.sqm.function.FunctionRegistry; 
 
 public class SQLiteDialect extends Dialect {
-    
-    // Empty constructor is fine
+
     public SQLiteDialect() {
+        registerColumnType(Types.BIT, "boolean");
+        registerColumnType(Types.TINYINT, "tinyint");
+        registerColumnType(Types.SMALLINT, "smallint");
+        registerColumnType(Types.INTEGER, "integer");
+        registerColumnType(Types.BIGINT, "bigint");
+        registerColumnType(Types.FLOAT, "float");
+        registerColumnType(Types.REAL, "real");
+        registerColumnType(Types.DOUBLE, "double");
+        registerColumnType(Types.NUMERIC, "numeric");
+        registerColumnType(Types.DECIMAL, "decimal");
+        registerColumnType(Types.CHAR, "char");
+        registerColumnType(Types.VARCHAR, "varchar");
+        registerColumnType(Types.LONGVARCHAR, "longvarchar");
+        registerColumnType(Types.DATE, "date");
+        registerColumnType(Types.TIME, "time");
+        registerColumnType(Types.TIMESTAMP, "timestamp");
+
+        registerFunction("concat",
+                new StandardSQLFunction("concat", StandardBasicTypes.STRING));
     }
 
-    // This method is now used to register functions in Hibernate 6
     @Override
-    protected void initializeFunctionRegistry(FunctionRegistry functionRegistry) {
-        super.initializeFunctionRegistry(functionRegistry);
-        
-        // Use functionRegistry.register and StandardBasicTypes.STRING
-        functionRegistry.register(
-            "lower", 
-            new StandardSQLFunction("lower", StandardBasicTypes.STRING)
-        );
+    public boolean supportsIdentityColumns() {
+        return true;
     }
 
     @Override
-    public boolean dropConstraints() {
+    public boolean hasDataTypeInIdentityColumn() {
         return false;
     }
 
     @Override
-    public boolean hasAlterTable() {
-        return false;
+    public String getIdentityColumnString() {
+        return "integer";
     }
 
     @Override
-    public boolean qualifyIndexName() {
-        return false;
+    public String getIdentitySelectString() {
+        return "select last_insert_rowid()";
     }
 }
