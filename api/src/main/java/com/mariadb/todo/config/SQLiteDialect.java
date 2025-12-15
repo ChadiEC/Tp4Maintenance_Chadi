1b4c6bb1a78a19015d82846178f9c545c7232d21
Chadi El Chami <1985081@collegemv.qc.ca>
2025-12-15 11:51:36 -0500

    passer a java 21

--- api/src/main/java/com/mariadb/todo/config/SQLiteDialect.java
+++ api/src/main/java/com/mariadb/todo/config/SQLiteDialect.java
@@ -7,20 +7,20 @@ import org.hibernate.query.sqm.function.FunctionRegistry;
 
 public class SQLiteDialect extends Dialect {
     
+    // Empty constructor is fine
+    public SQLiteDialect() {
+    }
 
+    // This method is now used to register functions in Hibernate 6
     @Override
-    protected void initializeFunctionRegistry(FunctionRegistry functionRegistry) { 
+    protected void initializeFunctionRegistry(FunctionRegistry functionRegistry) {
         super.initializeFunctionRegistry(functionRegistry);
         
+        // Use functionRegistry.register and StandardBasicTypes.STRING
         functionRegistry.register(
             "lower", 
             new StandardSQLFunction("lower", StandardBasicTypes.STRING)
         );
-    }
-
-    
-    public SQLiteDialect() {
-        
     }
 
     @Override
@@ -28,4 +28,13 @@ public class SQLiteDialect extends Dialect {
         return false;
     }
 
+    @Override
+    public boolean hasAlterTable() {
+        return false;
+    }
+
+    @Override
+    public boolean qualifyIndexName() {
+        return false;
+    }
 }
